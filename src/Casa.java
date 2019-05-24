649158ea02ed90bb981ef1418bcb6fb70fd682fc
gennakk <44756216+gennakk@users.noreply.github.com>
2019-05-24 13:26:15 +0200

    Nuevas Clases y proyecto
    Creacion de clase Casa.java

---
+++ src/Casa.java
@@ -0,0 +1,19 @@
+
+public class Casa {
+
+	private float superficie;
+	private String calle;
+	private int codigoPostal;
+	
+	/**
+	 * Un equipo de limpiadores deja la casa libre de germenes
+	 * y desinfecta todas las superficies
+	 */
+	public void limpiarCasa() {
+		String mensajito="Te hemos limpiado la casa estas arruinado";
+		System.out.println(mensajito);
+	}
+	
+	
+	
+}
